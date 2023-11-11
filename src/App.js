e8deda8f4410b54859643f837a9f9c3583762b08
ItryCode <sonuhrajeev@gmail.com>
2023-11-11 18:42:14 +0530

    Setup react router and boilerplates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import styled from "styled-components";
+import {
+  AboutPage,
+  AuthWrapper,
+  CartPage,
+  CheckoutPage,
+  ErrorPage,
+  HomePage,
+  PrivateRoute,
+  ProductsPage,
+  SingleProductPage,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+
+      <Routes>
+        <Route exact path="/" element={<HomePage />}></Route>
+        <Route exact path="/about" element={<AboutPage />} />
+
+        <Route exact path="/cart" element={<CartPage />} />
+
+        <Route exact path="/products" element={<ProductsPage />} />
+
+        <Route
+          exact
+          path="/products/:id"
+          children={SingleProductPage}
+          element={<SingleProductPage />}
+        />
+        <Route exact path="/checkout" element={<CheckoutPage />} />
+
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
