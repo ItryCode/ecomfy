7ababfde69a1c86bff6675dc0307497d829ed673
ItryCode <sonuhrajeev@gmail.com>
2023-11-12 22:26:20 +0530

    Setup up sidebar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 const Footer = () => {
-  return <h4>footer</h4>
-}
+  return (
+    <Wrapper>
+      <h5>
+        &copy; {new Date().getFullYear()}
+        <span> Ecomfy</span>
+      </h5>
+      <h5>All rights reserved</h5>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.footer`
   height: 5rem;
@@ -10,10 +18,10 @@ const Wrapper = styled.footer`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background: var(--clr-black);
+  background: var(--main-clr);
   text-align: center;
   span {
-    color: var(--clr-primary-5);
+    color: var(--sec-clr);
   }
   h5 {
     color: var(--clr-white);
@@ -26,6 +34,6 @@ const Wrapper = styled.footer`
   @media (min-width: 776px) {
     flex-direction: row;
   }
-`
+`;
 
-export default Footer
+export default Footer;
